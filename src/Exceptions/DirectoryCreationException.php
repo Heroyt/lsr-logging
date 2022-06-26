14b0a654d8c5a12b1ebc29f8293f757648685ea5
Tomáš Vojík <vojik@wboy.cz>
2022-06-26 16:30:12 +0200

    :tada: Initial commit

---
+++ src/Exceptions/DirectoryCreationException.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @author Tomáš Vojík <xvojik00@stud.fit.vutbr.cz>, <vojik@wboy.cz>
+ */
+
+namespace App\Logging\Exceptions;
+
+use Exception;
+use Throwable;
+
+/**
+ * Class DirectoryCreationException
+ *
+ * @package eSoul\Logging
+ */
+class DirectoryCreationException extends Exception
+{
+
+	public function __construct(string $path, Throwable $previous = null) {
+		parent::__construct(sprintf('Failed creating logging directory: %s', $path), 0, $previous);
+	}
+}
